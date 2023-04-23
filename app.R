a6d30737edac030d4a77ced27bf157c16323f6bf
Charlie Ikosi <charlieikosi@gmail.com>
2023-04-23 14:23:38 +1000

    RPostgreSQL Library

--- app.R
+++ app.R
@@ -1,7 +1,6 @@
-library(shiny)
 library(tidyverse)
-
 library(shiny)
+library(RPostgreSQL)
 
 ui <- fluidPage(titlePanel("Home Inventory"),
                 tags$head(tags$style(HTML('* { font-family: "Times New Roman"; }')))
